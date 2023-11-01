d41038246c76a858d2c9002807e78ce74da33ed3
ChrisCleaner <christoph.kruger@yahoo.com>
2023-11-01 16:27:04 +0100

    Add HF OpenSource Models
    Llama2 not tested to the end.

--- settings.py
+++ settings.py
@@ -20,14 +20,17 @@ EVAL_FILE_NAME = "eval.json"
 CHAIN_VERBOSITY = False
 
 #### the settings below can be used for testing ####
-# LLM_TYPE must be one of: "chatopenai", 
-LLM_TYPE = "chatopenai"
-# LLM_MODEL_TYPE must be one of: "gpt35", "gpt35_16", "gpt4", 
+# LLM_TYPE must be one of: "chatopenai", "hugging_face"
+LLM_TYPE = "hugging_face"
+# if LLM_TYPE is "chatopenai" then LLM_MODEL_TYPE must be one of: "gpt35", "gpt35_16", "gpt4"
+# if LLM_TYPE is "hugging_face" then LLM_MODEL_TYPE must be one of "llama2", "GoogleFlan"
+# "llama2" requires Huggingface Pro Account and access to the llama2 model https://huggingface.co/meta-llama/Llama-2-7b-chat-hf
+# note: llama2 is not fully tested, the last step was not undertaken, because no HF Pro account was available for the developer
 # Context window sizes are currently:
 # "gpt35": 4097 tokens which is equivalent to ~3000 words
 # "gpt35_16": 16385 tokens
 # "gpt4": 8192 tokens
-LLM_MODEL_TYPE = "gpt35"
+LLM_MODEL_TYPE = "GoogleFlan"
 # EMBEDDINGS_PROVIDER must be one of: "openai", 
 EMBEDDINGS_PROVIDER = "openai"
 # EMBEDDINGS_MODEL must be one of: "text-embedding-ada-002", 
